663a7697873b3a49546eeba09f65d9cc3b0f43d7
MacMaster <andrew.macmaster@fiserv.com>
2023-10-20 15:57:02 -0400

    Exercises 2-5, forgot to commit

--- src/main/java/com/xpanxion/assignments/student/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student/JavaOne.java
@@ -1,5 +1,5 @@
 package com.xpanxion.assignments.student;
-import java.util.Scanner;
+import java.util.*;
 
 public class JavaOne {
 
@@ -17,21 +17,70 @@ public class JavaOne {
         Scanner console = new Scanner(System.in);
         System.out.println("Enter your name: ");
         String name = console.nextLine();
-        System.out.println(name.toUpperCase());
-        console.close();Albert Huang
-        
+        System.out.print(name.toUpperCase());
+        console.close();
     }
 
     public void ex2() {
-        System.out.println("Student 1: ex2.");
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String sentence = console.nextLine();
+        int count = 0;
+        for (int i = 0; i < sentence.length(); i++)
+        {
+            if (Character.isUpperCase(sentence.charAt(i)))
+            {
+                count++;
+            }
+        }
+        System.out.println("Number of uppercase characters: " + count);
+        console.close();
     }
 
     public void ex3() {
-        System.out.println("Student 1: ex3.");
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String sentence = console.nextLine();
+
+        StringTokenizer tokenizer = new StringTokenizer(sentence);
+
+        String result = "";
+        int count = 0;
+        while (tokenizer.hasMoreTokens())
+        {
+            if (count % 2 == 0)
+            {
+                result += tokenizer.nextToken().toUpperCase();
+            }
+            else
+            {
+                result += tokenizer.nextToken();
+            }
+            count++;
+            result += " ";
+        }
+        System.out.println(result);
+        console.close();
+
     }
 
     public void ex4() {
-        System.out.println("Student 1: ex4.");
+        
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String pal = console.nextLine();
+
+        String reversed = new StringBuilder(pal).reverse().toString();
+
+        if (pal.equals(reversed))
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        console.close();
     }
 
     public void ex5() {
